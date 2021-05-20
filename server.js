01b0420b6f32ebc20afcfe5b7db5669aea6c48c1
Trenton <markhamtrenton@gmail.com>
2021-05-19 23:47:11 -0400

    added some stuff

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const hbs = exphbs.create({});
-
+const sequelize = require('./config/connection');
 const app = express();
-const PORT = process.env.PORT || 3110;
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./routes/forum-routes'));
 
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
 });
 
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}!`);
-// });;})
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});;})
