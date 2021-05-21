d03586ac39f2132246e55125326b562386b2a847
Lane Pemberton <lanebpemberton@gmail.com>
2021-05-20 20:41:29 -0400

    sql tweaks

--- routes/createThread.js
+++ routes/createThread.js
@@ -1,4 +1,12 @@
 module.exports = async function(req,res)
 {
+    //import sequelize
+    const Sequelize = require('sequelize');
+    //create connection to mysql datatbase
+    const sequelize = new Sequelize('mysql://root@localhost:3000/forum_db');
+    //create thread
+
+    //let initSequelize = new Sequelize( 'forum_db', 'root', '', {dialect :'postgres', port:'5432',schema:dbo});
+    //import something mysql related
     res.send("create thread route");
 }
