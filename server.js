8370c91220c1261aacc55de11bede8f96fc67ef5
Brent B <brentbaughan1@gmail.com>
2021-05-22 19:58:14 -0400

    updated server.js, connection.js for sequelize; updated .gitignore

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
 /* IMPORT DEPENDENCIES */
-//import custom env variables
-require('.env').config({path: __dirname + '/.env'})
-//import express
 const express = require('express');
-//import routes folder
+const session = require('express-session');
+const expresshandlebars = require('express-handlebars');
 const routes = require("./routes");
 
-/* APP INITIATION */
-//initiate app
-let app = express();
-//setup port number
-const port = process.env.PORT || 3306;
+//configure sequelize connection
+const sequelize = require('./config/connection');
+//connect sequelize to session stuff
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-/* ROUTE CONFIGURATION */
+
+const app = express();
+//setup port number
+const port = process.env.PORT || 4040;
+
+//startup handlebars
+const handlebars = expresshandlebars.create();
+
+//setup session tracking
+const sess = {
+    secret: "better have my money",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+}
+app.use(session(sess));
+
+
 //set up the express app to handle incoming data payload
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//make app use handlebars
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+/* ROUTE CONFIGURATION */
 //connect routes to app
-app.use("/routes",routes);  //sometimes i'll call "/routes" -> "/apis" 
+app.use(routes);  //sometimes i'll call "/routes" -> "/apis" 
 //setup port to listen on
-app.listen(port, () => console.log(`App listening on PORT ${port}`));
-
-
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log(`App listening on PORT ${port}`));
+})
 
 
 // mysql://
@@ -32,24 +55,21 @@ app.listen(port, () => console.log(`App listening on PORT ${port}`));
 // us-cdbr-east-03.cleardb.com
 
 
-const mysql = require('mysql');
-const config = require('./config');
-const ID = 'DbAgent:';
 
-const connection = mysql.createConnection({
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'beb8dda59d72ca',
-    password: '2e1a270b',
-    database: 'Forum_007_DB',
-    port: '3306',
-    queueLimit : 0,
-    connectionLimit : 0
- });
+// const ID = 'DbAgent:';
+
+// const connection = mysql.createConnection({
+//     host: 'us-cdbr-east-03.cleardb.com',
+//     user: 'beb8dda59d72ca',
+//     password: '2e1a270b',
+//     database: 'Forum_007_DB',
+//     port: '3306',
+//     queueLimit : 0,
+//     connectionLimit : 0
+//  });
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    else console.log(ID + 'now connected to Forum_DB');
-});
-
-module.exports = connection;
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     else console.log(ID + 'now connected to Forum_DB');
+// });
