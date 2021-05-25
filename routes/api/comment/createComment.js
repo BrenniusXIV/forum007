a76bfa5c5a72339cc575e9569538487133801c07
Brent B <brentbaughan1@gmail.com>
2021-05-25 19:26:37 -0400

    boilerplated comment and user routes

---
+++ routes/api/comment/createComment.js
@@ -0,0 +1,20 @@
+module.exports = async function(req,res)
+{
+    //import models folder
+    const {Thread} = require('../../../models');
+    try
+    {
+        const threadResults = await Thread.create({
+            ...req.body,
+            user_id: 1,
+            comment_section_id: 1,
+            board_id: 1,
+        });
+        //return thread results
+        res.status(200).json(threadResults);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({"error":error});
+    }
+
+}
