bb882ca789eb38537dff4ea66008d83ce6ee7bb4
Lane Pemberton <lanebpemberton@gmail.com>
2021-05-25 21:23:36 -0400

    finished initial update user route

--- routes/api/user/updateUser.js
+++ routes/api/user/updateUser.js
@@ -1,4 +1,23 @@
 module.exports = async function(req,res)
 {
-    res.send("udpate board route");
+    //import models folder
+    const {User} = require('../../../models');
+    //wrap dangerous code in try/catch
+    try
+    {
+        //update user
+        const userResults = await User.update(
+        {
+            ...req.body,
+        },
+        { 
+            where: { id: req.params.id }    //get user id from params
+        });
+        //return user results
+        res.status(200).json(userResults);
+    } catch (error)
+    {
+        //return error
+        res.status(500).json({"error":error});
+    }
 };
