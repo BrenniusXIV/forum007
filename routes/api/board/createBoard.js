d56a5e2186be0d9b39f7b8d3dbd1681c54786661
Ryan Simkoff <rsimkoff@gmail.com>
2021-05-26 16:10:39 -0400

    create board routes

--- routes/api/board/createBoard.js
+++ routes/api/board/createBoard.js
@@ -1,4 +1,24 @@
 module.exports = async function(req,res)
 {
-    res.send("create board route");
-};
+    // import models
+    const {Board} = require('../../../models');
+    
+    try
+    {
+       const boardResults = await Board.create({
+        ...req.body,
+        user_id: 1,
+        threads_id: 1,
+        comments_id: 1,
+        comments_section_id: 1,
+
+    });
+    //return board status/error
+    res.status(200).json(boardResults);
+    } catch (error) {
+    console.log(error);
+    res.status(500).json({"error,EEK!":error});
+}
+
+}
+module.exports = router;
