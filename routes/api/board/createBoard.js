89f8beb7442bd2013d5e0f36bbb85b4cc3708cbd
Ryan Simkoff <rsimkoff@gmail.com>
2021-05-26 21:38:18 -0400

    changes

--- routes/api/board/createBoard.js
+++ routes/api/board/createBoard.js
@@ -7,10 +7,10 @@ module.exports = async function(req,res)
     {
        const boardResults = await Board.create({
         ...req.body,
-        user_id: 1,
-        threads_id: 1,
-        comments_id: 1,
-        comments_section_id: 1,
+        // user_id: 1,
+        // threads_id: 1,
+        // comments_id: 1,
+        // comments_section_id: 1,
 
     });
     //return board status/error
--- routes/api/board/updateBoard.js
+++ routes/api/board/updateBoard.js
@@ -9,18 +9,18 @@ module.exports = async function(_req,res)
   const userResults = await Board.update(
     {
         ...req.body,
-        user_id: 1,
-        threads_id: 1,
-        comments_id: 1,
-        comments_section_id: 1,
+        // user_id: 1,
+        // threads_id: 1,
+        // comments_id: 1,
+        // comments_section_id: 1,
     },
     { 
          where: { 
              id: req.params.id,
-                user_id: req.session.user_id,
-             threads_id: req.session.thead_id,
-             comments_id: req.session.comments_id,
-             comments_section_id: req.session.comments_section_id,
+            //     user_id: req.session.user_id,
+            //  threads_id: req.session.thead_id,
+            //  comments_id: req.session.comments_id,
+            //  comments_section_id: req.session.comments_section_id,
     
             }    
     });
