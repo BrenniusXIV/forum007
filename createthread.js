9192ea9b916d45ed03a383de1977497f1df585b8
Trenton <markhamtrenton@gmail.com>
2021-05-28 17:13:19 -0400

    added create thread js css html handlebars

---
+++ createthread.js
@@ -0,0 +1,29 @@
+
+
+let threads = [];
+
+const createThread = (ev)=>{
+    ev.preventDefault();  //to stop the form submitting
+    let thread = {
+        id: Date.now(),
+        title: document.getElementById('thread-title').value,
+        content: document.getElementById('thread-content').value
+    }
+    threads.push(thread);
+    document.forms[0].reset(); // to clear the form for the next entries
+    //document.querySelector('form').reset();
+
+    //for display purposes only
+    console.warn('added' , {threads} );
+    let pre = document.querySelector('#msg pre');
+    pre.textContent = '\n' + JSON.stringify(threads, '\t', 2);
+
+    //saving to localStorage
+    localStorage.setItem('threadList', JSON.stringify(threads) );
+}
+// document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', createThread);
+
+
+
+
