037950e84269f08011b0f2cfbb876d6b83a9017e
Brent B <brentbaughan1@gmail.com>
2021-05-29 13:27:51 -0400

    re-added forum-routes entry in routes/index.js; commented out snow stuff on css

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 //require routes
 const api = require('./api');
+const forumRoutes = require('./forum-routes')
 //connect routes to router
 router.use("/api",api);
+router.use(forumRoutes);
 //export router
 module.exports = router;
