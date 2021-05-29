bdce924bc9913db0957b31ac9cd90d311ff82b53
Brent B <brentbaughan1@gmail.com>
2021-05-29 14:04:04 -0400

    not quite working, but board/:id route does lead somewhere now

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -1,33 +1,48 @@
-const router = require('express').Router();
-
+const router = require("express").Router();
+const { Board, Thread } = require("../models");
 const forum = [
-    {
-        id: 1,
-        thread_name: 'Gaming',
-    },
-    {
-        id: 2,
-        thread_name: 'Crypto',
-    },
-    {
-        id: 3,
-        thread_name: 'General',
-    },
-    {
-        id: 4,
-        thread_name: 'Programming',
-    },
-    {
-        title: "SPECTRE"
-    }
+  {
+    id: 1,
+    thread_name: "Gaming",
+  },
+  {
+    id: 2,
+    thread_name: "Crypto",
+  },
+  {
+    id: 3,
+    thread_name: "General",
+  },
+  {
+    id: 4,
+    thread_name: "Programming",
+  },
+  {
+    title: "SPECTRE",
+  },
 ];
 
-router.get('/', async (req, res) => {
-    res.render('all', {forum});
+router.get("/", async (req, res) => {
+  res.render("all", { forum });
 });
 
-router.get('/views/forum.handlebars/:id', async (req, res) => {
-    return res.render('forum', forum[req.params.id - 1]);
+router.get("/board/:id", async (req, res) => {
+  try {
+    const boardResult = await Board.findByPk(req.params.id, {
+      include: [
+        {
+          model: Thread,
+        },
+      ],
+    });
+    const board = boardResult.get({ plain: true });
+    res.render("board", ...board);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
+});
+router.get("/views/forum.handlebars/:id", async (req, res) => {
+  return res.render("forum", forum[req.params.id - 1]);
 });
 
 module.exports = router;
