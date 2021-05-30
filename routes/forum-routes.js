051f513f04f18ef76927f0b6869f7e925cecbe48
Brent B <brentbaughan1@gmail.com>
2021-05-29 20:58:20 -0400

    modified all.handlebars and forum-routes to display boards from back end

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -1,29 +1,19 @@
 const router = require("express").Router();
 const { Board, Thread, Comment, User } = require("../models");
-const forum = [
-  {
-    id: 1,
-    thread_name: "Gaming",
-  },
-  {
-    id: 2,
-    thread_name: "Crypto",
-  },
-  {
-    id: 3,
-    thread_name: "General",
-  },
-  {
-    id: 4,
-    thread_name: "Programming",
-  },
-  {
-    title: "SPECTRE",
-  },
-];
+
 
 router.get("/", async (req, res) => {
-  res.render("all", { forum });
+  try {
+    const boardsResult = await Board.findAll()
+
+    const boards = boardsResult.map((board) => board.get({plain:true}));
+    res.render("all", {
+      boards,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json({ "err": err });
+  }
 });
 
 router.get("/board/:id", async (req, res) => {
@@ -37,11 +27,10 @@ router.get("/board/:id", async (req, res) => {
       logged_in: req.session.logged_in
     });
   } catch (err) {
-    res.status(500).json({ err: err });
+    res.status(500).json({ "err": err });
   }
 });
-router.get("/views/forum.handlebars/:id", async (req, res) => {
-  return res.render("forum", forum[req.params.id - 1]);
-});
+
+
 
 module.exports = router;
