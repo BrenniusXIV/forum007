0140883e8c7b6f42bd868f553cd86175abcce232
Brent B <brentbaughan1@gmail.com>
2021-05-31 13:13:22 -0400

    corrected merge error in forum-routes

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -1,105 +1,109 @@
 const router = require("express").Router();
 const { Board, Thread, Comment, User } = require("../models");
 
-
 router.get("/", async (req, res) => {
   try {
-    const boardsResult = await Board.findAll()
+    const boardsResult = await Board.findAll();
 
-    const boards = boardsResult.map((board) => board.get({plain:true}));
+    const boards = boardsResult.map((board) => board.get({ plain: true }));
     res.render("all", {
       boards,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
-    res.status(500).json({ "err": err });
+    res.status(500).json({ err: err });
   }
 });
 
 router.get("/board/:id", async (req, res) => {
   try {
     const threadsResult = await Thread.findAll({
-      where: {board_id: req.params.id},
+      where: { board_id: req.params.id },
       include: [
         {
           model: User,
-          attributes: ['user_name'],
+          attributes: ["user_name"],
         },
       ],
     });
-    const threads = threadsResult.map((thread) => thread.get({plain:true}));
+    const threads = threadsResult.map((thread) => thread.get({ plain: true }));
     if (threads.length === 0) {
-      res.status(404).render("page404")
+      res.status(404).render("page404");
       return;
     }
     console.log(JSON.stringify(threads));
 
     //messy
-    for(let a in threads)
-    {
-        threads[a].preview = threads[a].body;
-        let charLength = 30;
-        if(threads[a].preview.length>charLength)
-        {
-            threads[a].preview = `${threads[a].preview.slice(0,charLength)}...`;
-        }
+    for (let a in threads) {
+      threads[a].preview = threads[a].body;
+      let charLength = 30;
+      if (threads[a].preview.length > charLength) {
+        threads[a].preview = `${threads[a].preview.slice(0, charLength)}...`;
+      }
     }
 
-
-router.get('/login', async (req, res) => {
-    res.render('login', {forum});
+    res.render("board", {
+      threads,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err });
+  }
 });
 
-router.get('/profile', async (req, res) => {
-    //get user data from session
-    try {
-        if (req.session.logged_in === true) {
-            const userData = await User.findOne({
-                where: {
-                    id: req.session.user_id
-                }
-            });
-            //serialize user data
-            const user = userData.get({plain:true});
-            //pass user to profile
-            res.render('profile', {
-                ...user,
-                logged_in:true
-            });
-        } else {
-            res.send(`You must be logged in to view your profile.`)
-        }
-    } catch (err) {
-        res.status(500).json({"error": err})
+router.get("/login", async (req, res) => {
+  res.render("login", { forum });
+});
+
+router.get("/profile", async (req, res) => {
+  //get user data from session
+  try {
+    if (req.session.logged_in === true) {
+      const userData = await User.findOne({
+        where: {
+          id: req.session.user_id,
+        },
+      });
+      //serialize user data
+      const user = userData.get({ plain: true });
+      //pass user to profile
+      res.render("profile", {
+        ...user,
+        logged_in: true,
+      });
+    } else {
+      res.send(`You must be logged in to view your profile.`);
     }
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
-
 
 router.get("/thread/:id", async (req, res) => {
-  const threadData = await Thread.findByPk(req.params.id, 
-    {
-      include: [
-        {
-          model: User,
-        },
-      ]
-    });
-  const commentData = await Comment.findAll({
-    where: {thread_id: req.params.id},
+  const threadData = await Thread.findByPk(req.params.id, {
     include: [
       {
         model: User,
-        attributes: ['user_name']
       },
-    ]
-  })
-  const comments = commentData.map((comment) => comment.get({plain: true}));
-  const thread = threadData.get({plain: true});
-  res.render('thread', {
+    ],
+  });
+  const commentData = await Comment.findAll({
+    where: { thread_id: req.params.id },
+    include: [
+      {
+        model: User,
+        attributes: ["user_name"],
+      },
+    ],
+  });
+  const comments = commentData.map((comment) => comment.get({ plain: true }));
+  const thread = threadData.get({ plain: true });
+  res.render("thread", {
     thread,
     comments,
-    logged_in: req.session.logged_in
-  })
-})
+    logged_in: req.session.logged_in,
+  });
+});
 
 module.exports = router;
